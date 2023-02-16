4c5cd2d7c615e443d0a1e6d3f86d1be2ddf56c90
romanches <cybernextgen@gmail.com>
2023-02-16 22:20:02 +0800

    daily

--- easy/20-valid-parentheses.py
+++ easy/20-valid-parentheses.py
@@ -8,19 +8,17 @@ class Solution:
     def isValid(self, input_str: str) -> bool:
         stack = []
         close_signs_mapping = {")": "(", "]": "[", "}": "{"}
-        for sign in input_str:
-            if sign in close_signs_mapping:
-                if stack:
+        try:
+            for sign in input_str:
+                if sign in close_signs_mapping:
                     last_open_sign = stack.pop()
                     if last_open_sign != close_signs_mapping[sign]:
                         return False
                     continue
-                return False
-            else:
                 stack.append(sign)
-        if stack:
+        except IndexError:
             return False
-        return True
+        return not stack
 
 
 class TestCase(unittest.TestCase):
