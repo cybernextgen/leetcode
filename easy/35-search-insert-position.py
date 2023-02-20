ba68fe5bb7c9d49a1d1bf8b58c2f953afc0e7271
romanches <cybernextgen@gmail.com>
2023-02-20 22:13:25 +0800

    daily

---
+++ easy/35-search-insert-position.py
@@ -0,0 +1,42 @@
+import unittest
+from typing import List
+
+data = (
+    ([1, 3, 5, 6], 5, 2),
+    ([1, 3, 5, 6], 2, 1),
+    ([1, 3, 5, 6], 7, 4),
+    ([1, 3, 5, 6], 0, 0),
+    ([1], 1, 0),
+    ([1, 3], 0, 0),
+)
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left_index = 0
+        right_index = len(nums) - 1
+        median_index = 0
+        median_element = 0
+        while left_index <= right_index:
+            median_index = left_index + (right_index - left_index) // 2
+            median_element = nums[median_index]
+            if median_element >= target:
+                right_index = median_index - 1
+                continue
+            left_index = median_index + 1
+        if median_element >= target:
+            return median_index
+        else:
+            return median_index + 1
+
+
+class TestCase(unittest.TestCase):
+    def test_solution(self):
+        s = Solution()
+
+        for input_data, target, result in data:
+            self.assertEqual(result, s.searchInsert(input_data, target))
+
+
+if __name__ == "__main__":
+    unittest.main()
